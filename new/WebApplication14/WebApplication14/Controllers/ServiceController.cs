acff7cf639ac83e0c8b72d078a1d9009e6aa7f09
Azat <46015824+serikovskii@users.noreply.github.com>
2019-11-22 22:08:29 +0600

    new project, test class comp

---
+++ new/WebApplication14/WebApplication14/Controllers/ServiceController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication14.DataAccess;
+using WebApplication14.Models;
+
+namespace WebApplication14.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        private readonly UserContext userContext;
+
+        public ServiceController(UserContext userContext)
+        {
+            this.userContext = userContext;
+        }
+
+        [HttpGet("{id}")]
+        public async Task <ActionResult<User>> GetUser(string id)
+        {
+            var oneUser = await userContext.Users.FindAsync(id);
+            if(oneUser == null)
+            {
+                return NotFound();
+            }
+            return oneUser;
+        }
+    }
+}
---
+++ new/WebApplication14/WebApplication14/Middleware/MyAuthMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication14.DataAccess;
+
+namespace WebApplication14.Middleware
+{
+    public class MyAuthMiddleware
+    {
+        private readonly RequestDelegate requestDelegate;
+
+        public MyAuthMiddleware(RequestDelegate requestDelegate)
+        {
+            this.requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //UserContext userContext = context.RequestServices.GetService(typeof(UserContext)) as UserContext;
+
+            UserContext userContext = context.RequestServices.GetService(typeof(UserContext)) as UserContext; 
+            var myHeaderKey = context.Request.Headers["MyHeaderKey"].ToString();
+            var path = context.Request.Path.ToString();
+            var users = userContext.Users.FirstOrDefault(key => key.MyHeaderKey == myHeaderKey);
+            if (path == "/api/auth" || users != null) 
+            {
+                await requestDelegate(context);
+            }
+            else
+                await context.Response.WriteAsync("Пользователь не авторизован");
+        }
+    }
+}
