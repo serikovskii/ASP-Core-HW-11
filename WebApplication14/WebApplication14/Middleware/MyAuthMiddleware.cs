8a8b0513ced90889bb55854575ffa1ea35ad67b8
Azat <46015824+serikovskii@users.noreply.github.com>
2019-11-22 17:57:19 +0600

    peredelat

--- WebApplication14/WebApplication14/Middleware/MyAuthMiddleware.cs
+++ WebApplication14/WebApplication14/Middleware/MyAuthMiddleware.cs
@@ -8,18 +8,19 @@ namespace WebApplication14.Middleware
     public class MyAuthMiddleware
     {
         private readonly RequestDelegate requestDelegate;
-        private readonly UserContext userContext;
 
-        public MyAuthMiddleware(RequestDelegate requestDelegate, UserContext userContext)
+        public MyAuthMiddleware(RequestDelegate requestDelegate)
         {
             this.requestDelegate = requestDelegate;
-            this.userContext = userContext;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-            var myHeaderKey = context.Request.Headers["My-HeaderKey"].ToString();
-            if (context.Request.Path == "api/auth/" || userContext.Users.FirstOrDefault(key => key.MyHeaderKey == myHeaderKey) != null)
+            UserContext userContext = context.RequestServices.GetService(typeof(UserContext)) as UserContext;
+            var myHeaderKey = context.Request.Headers["MyHeaderKey"].ToString();
+            var path = context.Request.Path.ToString();
+            var users = userContext.Users.FirstOrDefault(key => key.MyHeaderKey == myHeaderKey);
+            if (path == "/api/auth" || users != null) // 
             {
                 await requestDelegate(context);
             }
---
+++ WebApplication14/WebApplication14/Migrations/20191122115352_init2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication14.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "17c6292e-20ee-4536-b224-87cc76fdb1e1");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Login", "MyHeaderKey", "Name", "Password" },
+                values: new object[] { "409c68df-8784-459c-ba99-fbbe90e527db", "admin@admin.kz", "admin", "f89313a4-9ab7-4f5b-9cac-5364f0f20e53", "Azat", "1234" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "409c68df-8784-459c-ba99-fbbe90e527db");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Login", "MyHeaderKey", "Name", "Password" },
+                values: new object[] { "17c6292e-20ee-4536-b224-87cc76fdb1e1", "admin@admin.kz", "admin", "1a822e99-f9b2-4904-a730-0b97c98f0df5", "Azat", "1234" });
+        }
+    }
+}
